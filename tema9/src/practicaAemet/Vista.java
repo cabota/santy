b0396e8368bbaef39bb00e1afe8b65e9e34689e3
cabotaluis@gmail.com <cokelas94>
2019-05-27 18:53:57 +0200

    cabado vision

--- tema9/src/practicaAemet/Vista.java
+++ tema9/src/practicaAemet/Vista.java
@@ -5,11 +5,24 @@
  */
 package practicaAemet;
 
+import java.awt.Color;
+import java.awt.FlowLayout;
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.swing.JOptionPane;
+import javax.swing.JTable;
+import javax.swing.table.DefaultTableModel;
+import org.jfree.chart.*;
+import org.jfree.chart.plot.PlotOrientation;
+import org.jfree.data.category.DefaultCategoryDataset;
 
 /**
  *
@@ -19,7 +32,12 @@ public class Vista extends javax.swing.JFrame {
 static ArrayList<String>listProvincias;
 static ArrayList<String>listEstaciones;
 static ArrayList<String[]>idpro;
+static ArrayList<String[]>datos;
+static JTable jTable1 = new JTable();
+static JFreeChart chart ;
  Provincia p5;
+ 
+ static SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
     /**
      * Creates new form Vista
      */
@@ -53,8 +71,6 @@ static ArrayList<String[]>idpro;
         btnBuscar = new javax.swing.JButton();
         pnlEnglobaTodoAbajo = new javax.swing.JPanel();
         pnlGraficaGran = new javax.swing.JPanel();
-        jScrollPane1 = new javax.swing.JScrollPane();
-        tblDatos = new javax.swing.JTable();
         pnlGenerarGraf = new javax.swing.JPanel();
         pnlContenedorGenerarGraf = new javax.swing.JPanel();
         lblMagnitud = new javax.swing.JLabel();
@@ -151,52 +167,15 @@ static ArrayList<String[]>idpro;
 
         pnlGraficaGran.setBorder(new javax.swing.border.SoftBevelBorder(javax.swing.border.BevelBorder.RAISED));
 
-        tblDatos.setModel(new javax.swing.table.DefaultTableModel(
-            new Object [][] {
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null},
-                {null, null, null, null, null, null, null, null, null}
-            },
-            new String [] {
-                "Fecha", "Hora", "Temperatura", "Humedad", "Viento", "Direccion viento", "Nieve", "Lluvia", "Visibilidad"
-            }
-        ) {
-            boolean[] canEdit = new boolean [] {
-                true, true, true, true, false, true, true, true, true
-            };
-
-            public boolean isCellEditable(int rowIndex, int columnIndex) {
-                return canEdit [columnIndex];
-            }
-        });
-        jScrollPane1.setViewportView(tblDatos);
-
         javax.swing.GroupLayout pnlGraficaGranLayout = new javax.swing.GroupLayout(pnlGraficaGran);
         pnlGraficaGran.setLayout(pnlGraficaGranLayout);
         pnlGraficaGranLayout.setHorizontalGroup(
             pnlGraficaGranLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGap(0, 519, Short.MAX_VALUE)
-            .addGroup(pnlGraficaGranLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(pnlGraficaGranLayout.createSequentialGroup()
-                    .addContainerGap()
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.DEFAULT_SIZE, 495, Short.MAX_VALUE)
-                    .addContainerGap()))
         );
         pnlGraficaGranLayout.setVerticalGroup(
             pnlGraficaGranLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 445, Short.MAX_VALUE)
-            .addGroup(pnlGraficaGranLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, pnlGraficaGranLayout.createSequentialGroup()
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                    .addComponent(jScrollPane1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                    .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+            .addGap(0, 449, Short.MAX_VALUE)
         );
 
         pnlGenerarGraf.setBorder(javax.swing.BorderFactory.createEtchedBorder());
@@ -312,7 +291,7 @@ static ArrayList<String[]>idpro;
            // System.out.println(cmbListProvincias.getSelectedItem().toString());
                // rellenaComboEstacion(cmbListProvincias.getSelectedItem().toString());
                 
-                System.out.println(cmbListProvincias.getSelectedItem().toString());
+               
                  
                 for(int i =0;i<idpro.size();i++){
                     String[] idcompa= idpro.get(i);
@@ -328,10 +307,20 @@ static ArrayList<String[]>idpro;
 
     private void btnGenerarGraficaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnGenerarGraficaActionPerformed
         // TODO add your handling code here:
+        pnlContenedorGenerarGraf.setLayout(new FlowLayout());
+        
+        chart = this.crearGrafica();
+        ChartPanel CP = new ChartPanel(chart);
+         pnlContenedorGenerarGraf.add(CP);
+         pnlContenedorGenerarGraf.setVisible(true);
+        
+        
+       // pnlContenedorGenerarGraf.add(crearGrafica());
     }//GEN-LAST:event_btnGenerarGraficaActionPerformed
 
     private void cmbEstacionActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_cmbEstacionActionPerformed
         // TODO add your handling code here:
+        
     }//GEN-LAST:event_cmbEstacionActionPerformed
 
     private void txtDataFinActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_txtDataFinActionPerformed
@@ -340,6 +329,25 @@ static ArrayList<String[]>idpro;
 
     private void btnBuscarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_btnBuscarActionPerformed
         // TODO add your handling code here:
+        pnlGraficaGran.setLayout(new FlowLayout());
+        if(txtDataFin.getText().isEmpty()&& txtDataInici.getText().isEmpty()){
+            JOptionPane.showMessageDialog(null, "Las fechas no pueden estar vacias");
+        }else{
+            if(!validarFecha(txtDataFin.getText()) || !validarFecha(txtDataInici.getText())){
+                JOptionPane.showMessageDialog(null, "Las fechas Tiene que ser un formato YYYY-MM-DD");
+            }else{
+            
+            try {
+                Obtenerdatos(cmbEstacion.getSelectedItem().toString(), txtDataInici.getText(), txtDataFin.getText());
+                  pnlGraficaGran.add(jTable1);
+         pnlGraficaGran.setVisible(true);
+            } catch (ParseException ex) {
+                Logger.getLogger(Vista.class.getName()).log(Level.SEVERE, null, ex);
+            } catch (IOException ex) {
+                Logger.getLogger(Vista.class.getName()).log(Level.SEVERE, null, ex);
+            } 
+            }
+        }
     }//GEN-LAST:event_btnBuscarActionPerformed
 
     private void pnlContenedorGenerarGrafComponentShown(java.awt.event.ComponentEvent evt) {//GEN-FIRST:event_pnlContenedorGenerarGrafComponentShown
@@ -403,10 +411,71 @@ static ArrayList<String[]>idpro;
                   
                     //listIdProvincias.add(id[0]);
                    idpro.add(id);
-                   listProvincias.add(listaNombreProvincias);
+                   listProvincias.add(listaNombreProvincias.replaceAll("\"", ""));
 
                    
                }else{
+                   eof=true;
+               }
+               
+               
+           }
+           //System.out.println(listProvincias);
+           
+           archivoProvincas.close();
+           brListadoProvincias.close();
+           
+        }catch(FileNotFoundException e){
+            System.out.println("No se ha encontrado el doc.");;
+        }catch(IOException e){
+            System.out.println("Error en entrada o salida.");
+        }
+        
+        
+    } 
+         
+          public static void Obtenerdatos(String estacion,String fechaini, String fechafin) throws ParseException, IOException{
+        try {
+            FileReader archivoProvincas;
+            archivoProvincas = new FileReader("fichero/observaciones.csv");
+            BufferedReader brListadoProvincias = new BufferedReader(archivoProvincas);
+           boolean eof = false;
+            String lineaLectora;
+           
+           datos = new ArrayList<String[]>();
+        
+           while(!eof) {
+               lineaLectora=brListadoProvincias.readLine();
+               if(lineaLectora!=null  ){
+                   if(!lineaLectora.contains("ESPESOR_NIEVE")){
+                 
+//                   String listadoDone = listaNombreProvincias[1];  //Muestra la lista de los nombres
+//                   String listaDoneOk = listadoDone.replaceAll("\"", "");//Reemplaza "" por caracteres extraños
+
+                       String[] dat = lineaLectora.split(",");
+                       if(dat[2].contains(estacion)){
+                           Date fechain = sdf.parse(fechaini);
+                           Date fechafi=sdf.parse(fechafin);
+                           Date fechadoc=sdf.parse(dat[1].replaceAll("\"", ""));
+                           if(comprobarfecha(fechain,fechafi,fechadoc)){
+                               datos.add(dat);
+                           }
+                           
+                        
+                          
+                           
+                           
+                       }
+                       
+                       
+                   }
+                  
+                   
+                  
+
+                   
+               }else{
+                     ListarTabla(jTable1, datos);
                    eof=true;
                }
                
@@ -433,6 +502,17 @@ static ArrayList<String[]>idpro;
         }
     }
     
+    public static boolean validarFecha(String fecha) {
+        try {
+            SimpleDateFormat formatoFecha = new SimpleDateFormat("yyyy-MM-dd");
+            formatoFecha.setLenient(false);
+            formatoFecha.parse(fecha);
+        } catch (ParseException e) {
+            return false;
+        }
+        return true;
+    }
+    
      public static void enviarEstaciones() {
         
         for(String listaPro : listEstaciones){
@@ -440,6 +520,46 @@ static ArrayList<String[]>idpro;
             cmbEstacion.addItem(listaPro.toString());
         }
     }
+     
+     public JFreeChart crearGrafica() {
+
+DefaultCategoryDataset dataset = new DefaultCategoryDataset();
+
+dataset.setValue(3, "Presidente", "Enero");
+dataset.setValue(6, "Presidente", "Febrero");
+dataset.setValue(3, "Presidente", "Marzo");
+dataset.setValue(3, "Presidente", "Abril");
+dataset.setValue(8, "Presidente", "Mayo");
+dataset.setValue(10, "Presidente", "Junio");
+dataset.setValue(14, "Presidente", "Julio");
+                dataset.setValue(3, "Presidente", "Agosto");
+                dataset.setValue(3, "Presidente", "Septiembre");
+                dataset.setValue(15, "Presidente", "Octubre");
+                dataset.setValue(14, "Presidente", "Noviembre");
+                dataset.setValue(3, "Presidente", "Diciembre");
+
+dataset.setValue(6, "Ministro", "Enero");
+dataset.setValue(5, "Ministro", "Febrero");
+dataset.setValue(8, "Ministro", "Marzo");
+dataset.setValue(3, "Ministro", "Abril");
+dataset.setValue(7, "Ministro", "Mayo");
+dataset.setValue(6, "Ministro", "Junio");
+dataset.setValue(8, "Ministro", "Julio");
+                dataset.setValue(12, "Ministro", "Agosto");
+                dataset.setValue(3, "Ministro", "Septiembre");
+                dataset.setValue(14, "Ministro", "Octubre");
+                dataset.setValue(8, "Ministro", "Noviembre");
+                dataset.setValue(6, "Ministro", "Diciembre");
+
+JFreeChart grafi = ChartFactory.createLineChart("Comparativa de conferencias en un año", "Mes", "Conferencias", dataset,
+PlotOrientation.VERTICAL, true, true, false);
+
+
+grafi.getPlot().setBackgroundPaint(Color.BLUE);
+
+return grafi;
+
+}
     
     public static void rellenaComboEstacion(String provinciaSelected){
           
@@ -484,10 +604,54 @@ static ArrayList<String[]>idpro;
         }
         
     }
-    public static void enviarEstacion(){
-        
-        
+    
+    public static boolean comprobarfecha(Date in ,Date out,Date doc){   
+            if(doc.after(in) && doc.before(out) ){
+                    return true;
+            }else{
+                    return false;
+            }
     }
+    public static void ListarTabla(JTable tabla ,ArrayList<String[]> dat){
+    DefaultTableModel modeloTabla= new DefaultTableModel();
+    tabla.setModel(modeloTabla);
+
+    modeloTabla.addColumn("FECHA");
+    modeloTabla.addColumn("HORA");
+    modeloTabla.addColumn("temperatura");
+    modeloTabla.addColumn("Humedad");
+    modeloTabla.addColumn("Viento");
+    modeloTabla.addColumn("Direccion viento");
+    modeloTabla.addColumn("Nieve");
+    modeloTabla.addColumn("Lluvia");
+    modeloTabla.addColumn("Visibilidad");
+    
+
+    Object[] columna= new Object[9];
+
+    
+    
+    for (int i = 0; i < dat.size(); i++) {
+        String[] datpro = dat.get(i);       
+        String[] fechashora= datpro[1].split(" ");
+        columna[0]= fechashora[0];
+        columna[1]= fechashora[1];
+        columna[2]= datpro[10];
+        columna[3]= datpro[7];
+        columna[4]= datpro[4];
+        columna[5]= datpro[6];
+        columna[6]= datpro[14];
+        columna[7]= datpro[3];
+        columna[8]= datpro[13];
+        
+
+        modeloTabla.addRow(columna);
+      
+     }
+    jTable1=tabla;
+    
+   }
+  
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.JButton btnBuscar;
@@ -495,18 +659,16 @@ static ArrayList<String[]>idpro;
     private static javax.swing.JComboBox<String> cmbEstacion;
     private static javax.swing.JComboBox<String> cmbListProvincias;
     private javax.swing.JComboBox<String> cmbMagnitud;
-    private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JLabel lblDataFin;
     private javax.swing.JLabel lblDataInici;
     private javax.swing.JLabel lblEstacion;
     private javax.swing.JLabel lblMagnitud;
     private javax.swing.JLabel lblProvincia;
-    private javax.swing.JPanel pnlContenedorGenerarGraf;
+    private static javax.swing.JPanel pnlContenedorGenerarGraf;
     private javax.swing.JPanel pnlEleccionesBuscar;
     private javax.swing.JPanel pnlEnglobaTodoAbajo;
     private javax.swing.JPanel pnlGenerarGraf;
-    private javax.swing.JPanel pnlGraficaGran;
-    private javax.swing.JTable tblDatos;
+    private static javax.swing.JPanel pnlGraficaGran;
     private javax.swing.JTextField txtDataFin;
     private javax.swing.JTextField txtDataInici;
     // End of variables declaration//GEN-END:variables
